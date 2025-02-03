cae78418fd918a33972843aac6f00d6dabf35bb4
Ronald Sevilla <sevillaronald32@gmail.com>
2025-02-03 17:59:51 +0800

    first commit

---
+++ GameMap/Map.java
@@ -0,0 +1,78 @@
+package GameMap;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Map {
+    static String showMap(String currentLoc) {
+    String[] mapLoc = {"Room 1 ", "Room 2", "Room 3", "Room 4", "Room 5"};
+    String currentAd0 = "";
+    String currentAd1 = "";
+    String currentAd2 = "";
+    String currentAd3 = "";
+    String currentAd4 = "";
+    
+
+    int currentIndex = 0;
+    for (int i = 0; i < mapLoc.length; i++) {
+        if (currentLoc.equals(mapLoc[i])) {
+            currentIndex = i;
+            break;
+        }
+    }
+    switch (currentIndex) {
+        case 0:
+            currentAd0 = "x";
+            break;
+        case 1:
+            currentAd1 = "x";
+            break;
+        case 2:
+            currentAd2 = "x";
+            break;
+        case 3:
+            currentAd3 = "x";
+            break;
+        case 4:
+            currentAd4 = "x";
+            break;
+    }
+    String map = "" +
+                " ___________________                                                  ___________________ \n" +
+                "|                   |                                                |                   |     \n" +
+                "|       " + mapLoc[0] + "        |________________________________________________|       " + mapLoc[1] + "        |   \n" +
+                "|       (" + currentAd0 + ")         |________________________________________________|        (" + currentAd1 + ")         |  \n" +
+                "|                   |                                                |                   |  \n" +
+                "|___________________|                                                |___________________|  \n" +
+                "        |  |                                                                 |  |      \n" +
+                "        |  |                                                                 |  |      \n" +
+                "        |  |                                                                 |  |       \n" +
+                " _______|__|________                     ___________________          _______|__|________\n" +
+                "|                   |                   |                   |        |                   |  \n" +
+                "|      " + mapLoc[5] + "       |___________________|    " + mapLoc[4] + "    |        |      " + mapLoc[2] + "       |  \n" +
+                "|       (" + currentAd5 + ")          |___________________|       (" + currentAd4 + ")          |        |       (" + currentAd2 + ")          |   \n" +
+                "|                   |                   |                   |        |                   |   \n" +
+                "|___________________|                   |___________________|        |___________________|   \n" +
+                "                                                |  |                         |  |   \n" +
+                "                                                |  |                         |  |   \n" +
+                " ___________________                            |  |                         |  |   \n" +
+                "|                   |                           |  |                         |  |   \n" +
+                "|      " + mapLoc[3] + "       |___________________________|  |_________________________|  |  \n" +
+                "|       (" + currentAd3 + ")          |___________________________________________________________|  \n" +
+                "|                   |                      \n" +
+                "|___________________|          \n";
+
+    return map;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+        System.out.println(Map.showMap("Home"));
+    }
+}
+    
+
+
+   
