6e7fc5a3aa2df628a156ad02d7706e6187029412
Ronald Sevilla <sevillaronald32@gmail.com>
2025-02-04 21:01:45 +0800

    intro story

--- GameMap/Map.java
+++ GameMap/Map.java
@@ -4,6 +4,11 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Map {
+    static void println(String print) {
+        System.out.println(print);
+    }
+
+
     static String showMap(String currentLoc) {
     String[] mapLoc = {"Room 1 ", "Room 2", "Room 3", "Room 4", "Room 5"};
     String currentAd0 = "";
@@ -65,6 +70,13 @@ public class Map {
     return map;
 
     }
+    static void introduction(){
+        println("                               Coming from a poor family, I have always dreamed of becoming an engineer to provide a better life for my loved ones. \" +\r\n" + //
+                        "                       \"Today marks a crucial step toward that dream—the entrance exam for a prestigious public university. \" +\r\n" + //
+                        "                       \"To achieve my goal, I must pass a series of challenging exams that will test my knowledge and skills. \" +\r\n" + //
+                        "                       \"The road ahead won’t be easy, but I am determined to succeed.");
+        
+    }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -98,16 +110,35 @@ public class Map {
 
 
 
-        System.out.println("                                                 "+text);
-        System.out.println("  ___                     _   _        _                      _____             _                      ");
-        System.out.println(" |_ _|_      ____ _ _ __ | |_| |_ ___ | |__   ___  __ _ _ __ | ____|_ __   __ _(_)_ __   ___  ___ _ __ ");
-        System.out.println("  | |\\ \\ /\\ / / _` | '_ \\| __| __/ _ \\| '_ \\ / _ \\/ _` | '_ \\|  _| | '_ \\ / _` | | '_ \\ / _ \\/ _ \\ '__|");
-        System.out.println("  | | \\ V  V / (_| | | | | |_| || (_) | |_) |  __/ (_| | | | | |___| | | | (_| | | | | |  __/  __/ |   ");
-        System.out.println(" |___| \\_/\\_/ \\__,_|_| |_|\\__|\\__\\___/|_.__/ \\___|\\__,_|_| |_|_____|_| |_|\\__, |_|_| |_|\\___|\\___|_|   ");
-        System.out.println("                                                                          |___/                        ");
+        println("                                                 "+text);
+        println("  ___                     _   _        _                      _____             _                      ");
+        println(" |_ _|_      ____ _ _ __ | |_| |_ ___ | |__   ___  __ _ _ __ | ____|_ __   __ _(_)_ __   ___  ___ _ __ ");
+        println("  | |\\ \\ /\\ / / _` | '_ \\| __| __/ _ \\| '_ \\ / _ \\/ _` | '_ \\|  _| | '_ \\ / _` | | '_ \\ / _ \\/ _ \\ '__|");
+        println("  | | \\ V  V / (_| | | | | |_| || (_) | |_) |  __/ (_| | | | | |___| | | | (_| | | | | |  __/  __/ |   ");
+        println(" |___| \\_/\\_/ \\__,_|_| |_|\\__|\\__\\___/|_.__/ \\___|\\__,_|_| |_|_____|_| |_|\\__, |_|_| |_|\\___|\\___|_|   ");
+        println("                                                                          |___/                        ");
         
+        println("1.Start");
+        println("2.How to Play");
+        println("3.About");
+        println("Choose key to proceed: ");
+        int choice = scan.nextInt();
+
         
-        System.out.println(Map.showMap("Home"));
+        switch (choice) {
+            case 1:
+                introduction();
+                println(Map.showMap("Room 1"));
+                break;
+            case 2:
+                println("How to Play");
+            case 3:
+                println("About");
+                break;
+            default:
+                break;
+        }
+       
     }
 }
     
